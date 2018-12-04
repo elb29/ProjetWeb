36dce5af7d84216075e1d0f7becf1acecc1e815d
elb29 <45095743+elb29@users.noreply.github.com>
2018-12-04 15:52:00 +0100

    Add files via upload

--- projetweb.sql
+++ projetweb.sql
@@ -1,34 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1
--- Généré le :  lun. 03 déc. 2018 à 09:27
--- Version du serveur :  5.7.17
--- Version de PHP :  5.6.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données :  `projetweb`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `objets`
---
-
-CREATE TABLE `objets` (
+﻿CREATE TABLE `objets` (
   `id` int(11) NOT NULL,
   `nom` text,
   `lat` double NOT NULL,
@@ -75,8 +45,3 @@ INSERT INTO `scores` (`pseudo`, `temps`) VALUES
 ('Etienne', '06:59:59'),
 ('Etienne', '01:52:00'),
 ('Etienne', '00:01:48');
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
